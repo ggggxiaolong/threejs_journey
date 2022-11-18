45f0a8447fe857ec0186a2e57409fcef8d85cf43
MrTan <freemrtan@gmail.com>
2022-11-18 18:04:11 +0800

    05-transorms-objects

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -5,13 +5,47 @@ const canvas = document.querySelector(".webgl");
 // console.log(canvas)
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0.7, -0.6, 1);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: "red" });
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.set(0.7, -0.6, 1);
+// scene.add(mesh);
 
-mesh.scale.set(2, 0.5, 0.5)
+// mesh.scale.set(2, 0.5, 0.5);
+
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2
+
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2
+
+group.add(cube3)
+
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
 
 const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper);
