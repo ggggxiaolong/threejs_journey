9ab05aee21794b7df6c855f52ee355aa5f5162fa
MrTan <freemrtan@gmail.com>
2022-11-19 22:14:26 +0800

    06-animation

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,54 +1,19 @@
 import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
 
+// console.log(gsap)
 const canvas = document.querySelector(".webgl");
-// console.log(canvas)
 const scene = new THREE.Scene();
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: "red" });
-// const mesh = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
 // mesh.position.set(0.7, -0.6, 1);
-// scene.add(mesh);
+scene.add(mesh);
 
-// mesh.scale.set(2, 0.5, 0.5);
-
-// mesh.rotation.reorder("YXZ");
-// mesh.rotation.x = Math.PI * 0.25;
-// mesh.rotation.y = Math.PI * 0.25;
-
-const group = new THREE.Group();
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-cube2.position.x = -2
-
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff })
-);
-cube3.position.x = 2
-
-group.add(cube3)
-
-group.position.y = 1
-group.scale.y = 2
-group.rotation.y = 1
-
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 const size = {
   width: 800,
@@ -56,7 +21,7 @@ const size = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 3;
+camera.position.z = 4;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -64,4 +29,15 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(size.width, size.height);
 
-renderer.render(scene, camera);
+// const clock = new THREE.Clock();
+gsap.to(mesh.position, {x: 2, duration: 1, delay: 1});
+gsap.to(mesh.position, {x: 1, duration: 1, delay: 2});
+
+function tick() {
+  // mesh.rotation.y = clock.getElapsedTime();
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+}
+
+tick();
