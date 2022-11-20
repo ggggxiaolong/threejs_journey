5d769f96505f13d904e236670e26938fe5ea7ae2
MrTan <freemrtan@gmail.com>
2022-11-20 22:17:49 +0800

    07-cameras

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,12 +1,13 @@
 import "./style.css";
 import * as THREE from "three";
 import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // console.log(gsap)
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
 const mesh = new THREE.Mesh(geometry, material);
 // mesh.position.set(0.7, -0.6, 1);
@@ -20,8 +21,19 @@ const size = {
   height: 600,
 };
 
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 4;
+// const aspectRatio = size.width / size.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  size.width / size.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -30,11 +42,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(size.width, size.height);
 
 // const clock = new THREE.Clock();
-gsap.to(mesh.position, {x: 2, duration: 1, delay: 1});
-gsap.to(mesh.position, {x: 1, duration: 1, delay: 2});
+// gsap.to(mesh.position, {x: 2, duration: 1, delay: 1});
+// gsap.to(mesh.position, {x: 1, duration: 1, delay: 2});
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 function tick() {
   // mesh.rotation.y = clock.getElapsedTime();
+  controls.update();
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
