2d53d4876fd29acf3be352b2f4a020ea2c3f3268
MrTan <freemrtan@gmail.com>
2022-11-30 11:00:52 +0800

    20 Raycaster

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -2,108 +2,66 @@ import "./style.css";
 import * as THREE from "three";
 // import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import * as dat from "dat.gui";
+// import * as dat from "dat.gui";
 
-const gui = new dat.GUI();
+// const gui = new dat.GUI();
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
 
-// const loader = new THREE.TextureLoader();
+// const material =
+const geometry = new THREE.SphereGeometry(0.5, 16, 16);
+const sphere1 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: "#FB7299" })
+);
+scene.add(sphere1);
+const sphere2 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: "#FB7299" })
+);
+sphere2.position.x = -2;
+scene.add(sphere2);
+const sphere3 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: "#FB7299" })
+);
+sphere3.position.x = 2;
+scene.add(sphere3);
 
-// cube gui
-// size, count,
-// line radius
-// branch
-// random
-// color
-const parameter = {};
-parameter.count = 30000;
-parameter.size = 0.02;
-parameter.radius = 5;
-parameter.branch = 3;
-parameter.randomness = 5;
-parameter.randomnessPow = 2.5;
-parameter.spin = 1.2;
-parameter.innerColor = '#d43a3a'
-parameter.outerColor = '#252598'
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const direction = new THREE.Vector3(10, 0, 0);
+// direction.normalize();
+// const raycaster = new THREE.Raycaster(rayOrigin, direction);
+let focusSphere = null;
+let oldFocusSphere = null;
+const mouse = new THREE.Vector2();
 
-gui.add(parameter, "count", 100, 100000, 10).onFinishChange(generateGalaxy);
-gui.add(parameter, "size", 0.01, 1, 0.001).onFinishChange(generateGalaxy);
-gui.add(parameter, "radius", 1, 10, 1).onFinishChange(generateGalaxy);
-gui.add(parameter, "branch", 1, 10, 1).onFinishChange(generateGalaxy);
-gui.add(parameter, "randomness", 0, 5, 0.01).onFinishChange(generateGalaxy);
-gui.add(parameter, "randomnessPow", 1, 10, 0.1).onFinishChange(generateGalaxy);
-gui.add(parameter, "spin", -5, 5, 0.1).onFinishChange(generateGalaxy);
-gui.addColor(parameter, "innerColor").onFinishChange(generateGalaxy);
-gui.addColor(parameter, "outerColor").onFinishChange(generateGalaxy);
+const raycaster = new THREE.Raycaster();
+const spheres = [sphere1, sphere2, sphere3];
 
-let geometry = null;
-let material = null;
-let points = null;
-
-function generateGalaxy() {
-  if (geometry != null) {
-    geometry.dispose();
-    material.dispose();
-    scene.remove(points);
-  }
-
-  geometry = new THREE.BufferGeometry();
-  material = new THREE.PointsMaterial({
-    size: parameter.size,
-    depthWrite: false,
-    blending: THREE.AdditiveBlending,
-    vertexColors: true
-  });
-  const positions = new Float32Array(parameter.count * 3);
-  const colors = new Float32Array(parameter.count * 3);
-  const brachAngle = (Math.PI * 2) / parameter.branch;
-  const innerColor = new THREE.Color(parameter.innerColor)
-  const outerColor = new THREE.Color(parameter.outerColor)
-
-  for (let i = 0; i < parameter.count; i++) {
-    const i3 = i * 3;
-    const random = Math.random();
-    // const fixRandom = random - 0.5
-    const radius = random * parameter.radius;
-    const spinAngle = radius * parameter.spin;
-    const branchIndex = i % parameter.branch;
-    positions[i3] = 
-      radius * Math.cos(branchIndex * brachAngle + spinAngle) +
-      Math.pow(Math.random(), parameter.randomnessPow) *
-        (Math.random() > 0.5 ? 1 : -1);
-    positions[i3 + 1] = 
-      Math.pow(Math.random(), parameter.randomnessPow) *
-      (Math.random() > 0.5 ? 1 : -1);
-    positions[i3 + 2] = 
-      radius * Math.sin(branchIndex * brachAngle + spinAngle) +
-      Math.pow(Math.random(), parameter.randomnessPow) *
-        (Math.random() > 0.5 ? 1 : -1);
-    const mixColor = innerColor.clone()
-    mixColor.lerp(outerColor, random)
-    colors[i3] = mixColor.r
-    colors[i3 + 1] = mixColor.g
-    colors[i3 + 2] = mixColor.b
-  }
-
-  geometry.setAttribute(
-    "position",
-    new THREE.Float32BufferAttribute(positions, 3)
-  );
-  geometry.setAttribute(
-    "color",
-    new THREE.Float32BufferAttribute(colors, 3)
-  );
-  points = new THREE.Points(geometry, material);
-  scene.add(points);
-}
-
-generateGalaxy();
+const intersectObjects = raycaster.intersectObjects(spheres);
+console.log(intersectObjects.length);
 
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / size.width) * 2 - 1;
+  mouse.y = (event.clientY / size.height) * -2 + 1;
+  // console.log(`X: ${mouse.x}, Y: ${mouse.y}`)
+});
+
+window.addEventListener("click", () => {
+  for (const sphere of spheres) {
+    if (focusSphere.object === sphere) {
+      sphere.material.color.set("#00AEEC");
+    } else {
+      sphere.material.color.set("#FB7299");
+    }
+  }
+});
 
 window.addEventListener("resize", () => {
   size.width = window.innerWidth;
@@ -121,7 +79,7 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 
-camera.position.set(0, 8, 0);
+camera.position.set(0, 0, 5);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -139,7 +97,27 @@ controls.enableDamping = true;
 const clock = new THREE.Clock();
 
 function tick() {
-  // const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
+  sphere1.position.y = Math.sin(elapsedTime * 0.3);
+  sphere2.position.y = Math.sin(elapsedTime * 0.8);
+  sphere3.position.y = Math.sin(elapsedTime * 1.3);
+  raycaster.setFromCamera(mouse, camera);
+  const intersectObjects = raycaster.intersectObjects(spheres);
+  // for (const sphere of spheres) {
+  //   sphere.material.color.set("#FB7299");
+  // }
+
+  // for (const obj of intersectObjects) {
+  //   obj.object.material.color.set("#00AEEC");
+  // }
+  if (intersectObjects.length != 0 && focusSphere == null) {
+    focusSphere = intersectObjects[0];
+  }
+  if (intersectObjects.length == 0 && focusSphere != null) {
+    oldFocusSphere = focusSphere;
+    focusSphere = null;
+  }
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
