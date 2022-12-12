bd4f7401c40b9acf3290376e672b81d1973adef7
MrTan <freemrtan@gmail.com>
2022-12-12 18:05:10 +0800

    shaders

--- 04-webpack/bundler/webpack.common.js
+++ 04-webpack/bundler/webpack.common.js
@@ -76,7 +76,14 @@ module.exports = {
                 {
                     filename: 'assets/fonts/[hash][ext]'
                 }
-            }
+            },
+
+            // shader
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: ['raw-loader'],
+            },
         ]
     }
 }
--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,4 +1,57 @@
 import "./style.css";
-import {Experience} from "./experience/experience"
-const canvas = document.querySelector(".webgl");
-const experience = new Experience(canvas)
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  RawShaderMaterial,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
+
+const canvas = document.querySelector("canvas.webgl");
+const scene = new Scene();
+
+const material = new RawShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+});
+const flagMesh = new Mesh(new PlaneGeometry(1.5, 1.5), material);
+scene.add(flagMesh);
+
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
+};
+
+const camera = new PerspectiveCamera(75, size.width / size.height, 0.01, 100);
+camera.position.set(0, 0, 3);
+scene.add(camera);
+const render = new WebGLRenderer({ canvas: canvas, antialias: true });
+render.setSize(size.width, size.height);
+render.setPixelRatio(size.devicePixelRatio);
+
+const control = new OrbitControls(camera, canvas);
+
+window.addEventListener("resize", () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  size.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+  render.setSize(size.width, size.height);
+  render.setPixelRatio(size.devicePixelRatio);
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+});
+
+function update() {
+  render.render(scene, camera);
+  control.update();
+  window.requestAnimationFrame(update);
+}
+update();
