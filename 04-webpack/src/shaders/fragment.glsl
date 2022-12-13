751c703d760cbd4647ea1c1a5a0cf2d9ee6a4736
MrTan <freemrtan@gmail.com>
2022-12-13 15:35:42 +0800

    27 shaders

--- 04-webpack/src/shaders/fragment.glsl
+++ 04-webpack/src/shaders/fragment.glsl
@@ -1,5 +1,46 @@
+#ifdef GL_ES
 precision mediump float;
+#endif
 
-void main(){
-    gl_FragColor = vec4(1.0, 1.0, 0, 1.0);
+uniform float u_time;
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform vec3 u_color;
+uniform sampler2D u_texture;
+
+varying vec2 v_uv;
+varying float v_elevation;
+// varying vec4 v_position;
+// varying float vRandom;
+
+#define PI 3.14159265359
+
+// vec3 colorA = vec3(0.149, 0.141, 0.912);
+// vec3 colorB = vec3(1.000, 0.833, 0.224);
+
+// float plot(vec2 st, float pct) {
+//     return smoothstep(pct - 0.01, pct, st.y) - smoothstep(pct, pct + 0.01, st.y);
+// }
+
+// vec3 hsb2rgb( in vec3 c ){
+//     vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+//                              6.0)-3.0)-1.0,
+//                      0.0,
+//                      1.0 );
+//     rgb = rgb*rgb*(3.0-2.0*rgb);
+//     return c.z * mix(vec3(1.0), rgb, c.y);
+// }
+
+void main() {
+    // vec2 st = v_uv;
+    // vec3 color = vec3(0.5);
+    // vec2 bl = smoothstep(vec2(0.0, 0.0) ,vec2(0.1), st);
+    // float pct = bl.x * bl.y;
+    // vec2 tr = smoothstep(vec2(1.0, 1.0),vec2(0.1),1.0-st);
+    // pct *= tr.x * tr.y;
+    // color = vec3(pct);
+    vec4 color = texture2D(u_texture, v_uv);
+    color.xyz *= v_elevation * 0.5 + 0.9;
+    gl_FragColor = color;
+    // gl_FragColor = ;
 }
--- 04-webpack/src/shaders/vertex.glsl
+++ 04-webpack/src/shaders/vertex.glsl
@@ -1,9 +1,22 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+uniform float u_time;
+uniform vec2 u_frequency;
 
-attribute vec3 position;
+#define PI 3.14159265359
 
-void main(){
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+attribute float aRandom;
+varying vec2 v_uv;
+varying float v_elevation;
+// varying vec4 v_position;
+
+void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    float elevation = sin(modelPosition.x * PI * u_frequency.x + modelPosition.y * u_frequency.y - u_time) * 0.2;
+    modelPosition.z = elevation;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectPosition = projectionMatrix * viewPosition;
+    // gl_Position = projectionMatrix * viewMatrix * modelPosition;
+    gl_Position = projectPosition;
+    // v_position = projectPosition;
+    v_uv = uv;
+    v_elevation = elevation;
 }
