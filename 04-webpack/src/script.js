8b12cffe607b2336620496547a4ded1dff88a418
MrTan <freemrtan@gmail.com>
2022-12-13 21:05:25 +0800

    28 Shader patterns

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,21 +1,14 @@
 import "./style.css";
 import {
-  BoxGeometry,
-  BufferAttribute,
-  Clock,
   Color,
   Mesh,
-  MeshBasicMaterial,
   PerspectiveCamera,
   PlaneGeometry,
-  RawShaderMaterial,
   Scene,
   ShaderMaterial,
-  TextureLoader,
-  Vector2,
   WebGLRenderer,
 } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 import { GUI } from "dat.gui";
@@ -26,46 +19,22 @@ const size = {
   pixelRatio: Math.min(window.devicePixelRatio, 2),
 };
 
-// console.log(size);
-const loader = new TextureLoader();
 const canvas = document.querySelector("canvas.webgl");
 const scene = new Scene();
-const clock = new Clock();
-const mouse = new Vector2();
 const gui = new GUI();
-const params = {};
-params.frequency = 3.0;
-
-const flagTexture = loader.load("/textures/frag.jpg");
 
 const material = new ShaderMaterial({
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
   uniforms: {
-    u_time: { type: "f", value: clock.getElapsedTime() },
-    u_resolution: { type: "v2", value: new Vector2(size.width, size.height) },
-    u_mouse: { value: mouse },
     u_color: { value: new Color("#ff00ff") },
-    u_frequency: { value: new Vector2(2, 1) },
-    u_texture: { value: flagTexture },
   },
 });
-gui
-  .add(material.uniforms.u_frequency.value, "x", 1, 10, 0.1)
-  .name("frequencyX");
-gui
-  .add(material.uniforms.u_frequency.value, "y", 0, 10, 0.1)
-  .name("frequencyY");
+
 
 const geomtry = new PlaneGeometry(1.5, 1.5, 32, 32);
 const flagMesh = new Mesh(geomtry, material);
 scene.add(flagMesh);
-const count = geomtry.attributes.position.array.length;
-const randomArray = new Float32Array(count);
-for (let i = 0; i < count; i++) {
-  randomArray[i] = Math.random();
-}
-geomtry.setAttribute("aRandom", new BufferAttribute(randomArray, 1));
 
 const camera = new PerspectiveCamera(75, size.width / size.height, 0.01, 100);
 camera.position.set(0, 0, 3);
@@ -74,11 +43,6 @@ const render = new WebGLRenderer({ canvas: canvas, antialias: true });
 render.setSize(size.width, size.height);
 render.setPixelRatio(size.devicePixelRatio);
 render.setClearColor(0x333333);
-
-const control = new OrbitControls(camera, canvas);
-control.enableDamping = true;
-// control.enablePan = true;
-// control.enabled = false;
 
 window.addEventListener("resize", () => {
   size.width = window.innerWidth;
@@ -89,20 +53,11 @@ window.addEventListener("resize", () => {
   render.setPixelRatio(size.devicePixelRatio);
   camera.aspect = size.width / size.height;
   camera.updateProjectionMatrix();
-  material.uniforms.u_resolution.x = size.width;
-  material.uniforms.u_resolution.y = size.height;
 });
 
-window.addEventListener("mousemove", (e) => {
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-  material.uniforms.u_mouse.value = mouse;
-});
 
 function update() {
   render.render(scene, camera);
-  control.update();
-  material.uniforms.u_time.value = clock.getElapsedTime();
   window.requestAnimationFrame(update);
 }
 update();
--- 04-webpack/src/shaders/fragment.glsl
+++ 04-webpack/src/shaders/fragment.glsl
@@ -2,45 +2,171 @@
 precision mediump float;
 #endif
 
-uniform float u_time;
-uniform vec2 u_resolution;
-uniform vec2 u_mouse;
 uniform vec3 u_color;
-uniform sampler2D u_texture;
 
-varying vec2 v_uv;
-varying float v_elevation;
-// varying vec4 v_position;
-// varying float vRandom;
+varying vec2 vUv;
 
 #define PI 3.14159265359
 
-// vec3 colorA = vec3(0.149, 0.141, 0.912);
-// vec3 colorB = vec3(1.000, 0.833, 0.224);
+float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) *
+        43758.5453123);
+}
 
-// float plot(vec2 st, float pct) {
-//     return smoothstep(pct - 0.01, pct, st.y) - smoothstep(pct, pct + 0.01, st.y);
-// }
+vec4 permute(vec4 x)
+{
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
 
-// vec3 hsb2rgb( in vec3 c ){
-//     vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
-//                              6.0)-3.0)-1.0,
-//                      0.0,
-//                      1.0 );
-//     rgb = rgb*rgb*(3.0-2.0*rgb);
-//     return c.z * mix(vec3(1.0), rgb, c.y);
-// }
+//	Classic Perlin 2D Noise 
+//	by Stefan Gustavson
+//
+vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec2 P){
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+  vec4 i = permute(permute(ix) + iy);
+  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+  vec4 gy = abs(gx) - 0.5;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+  vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
 
 void main() {
-    // vec2 st = v_uv;
-    // vec3 color = vec3(0.5);
-    // vec2 bl = smoothstep(vec2(0.0, 0.0) ,vec2(0.1), st);
-    // float pct = bl.x * bl.y;
-    // vec2 tr = smoothstep(vec2(1.0, 1.0),vec2(0.1),1.0-st);
-    // pct *= tr.x * tr.y;
-    // color = vec3(pct);
-    vec4 color = texture2D(u_texture, v_uv);
-    color.xyz *= v_elevation * 0.5 + 0.9;
-    gl_FragColor = color;
-    // gl_FragColor = ;
+    float partY = mod(vUv.y * 10.0, 1.0);
+    float partX = mod(vUv.x * 10.0, 1.0);
+    float strength = step(0.8, partX) * step(0.55, partY) +
+        step(0.8, partX) * (1.0 - step(0.25, partY)) +
+        step(0.55, partX) * step(0.8, partY) +
+        (1.0 - step(0.25, partX)) * step(0.8, partY);
+    // --------------------------------------------------------
+    // float partX = abs( vUv.x * 2.0 - 1.0);
+    // float partY = abs( vUv.y * 2.0 - 1.0);
+
+    // float strength = min(partX, partY);
+    // --------------------------------------------------------
+    // float partX = abs( vUv.x * 2.0 - 1.0);
+    // float partY = abs( vUv.y * 2.0 - 1.0);
+
+    // float strength = max(partX, partY);
+    // --------------------------------------------------------
+    // float partX = abs( vUv.x * 2.0 - 1.0);
+    // float partY = abs( vUv.y * 2.0 - 1.0);
+
+    // float strength = step(0.5,partX) + step(0.5,partY);
+    // --------------------------------------------------------
+    // float partX = abs( vUv.x * 2.0 - 1.0);
+    // float partY = abs( vUv.y * 2.0 - 1.0);
+
+    // float strength = - step(0.7,max(partX, partY)) + step(0.5,max(partX, partY));
+    // --------------------------------------------------------
+    // float strength = (ceil(vUv.x * 11.0) - 1.0) / 10.0;
+    // --------------------------------------------------------
+    // float partX = floor(vUv.x * 10.0) / 10.0;
+    // float partY = floor(vUv.y * 10.0) / 10.0;
+    // float strength = partY * partX;
+    // --------------------------------------------------------
+    // float partX = floor(vUv.x * 10.0) / 10.0;
+    // float partY = floor(vUv.y * 10.0) / 10.0;
+    // float strength = partY * partX;
+    // --------------------------------------------------------
+    // vec2 colors = vec2(
+    //     floor(vUv.x * 10.0) / 10.0, 
+    //     floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0);
+    // float strength = random(colors);
+    // --------------------------------------------------------
+    // float strength = length(vUv);
+    // --------------------------------------------------------
+    // float strength = distance(vUv, vec2(0.5));
+    // --------------------------------------------------------
+    // float strength = 1.0 - distance(vUv, vec2(0.5));
+    // --------------------------------------------------------
+    // float strength = 0.05 / distance(vUv, vec2(0.5));
+    // --------------------------------------------------------
+    // float strength = step(0.25, distance(vUv, vec2(0.5)));
+    // --------------------------------------------------------
+    // float strength = abs(distance(vUv, vec2(0.5)) - 0.25);
+    // --------------------------------------------------------
+    // float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+    // --------------------------------------------------------
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+    // --------------------------------------------------------
+    // vec2 vUv2 = vec2(
+    //     vUv.x,
+    //     vUv.y + sin(vUv.x * 10.0 * PI) * 0.1
+    // );
+    // float strength = 1.0 - step(0.01, abs(distance(vUv2, vec2(0.5)) - 0.25));
+    // --------------------------------------------------------
+    // vec2 vUv2 = vec2(
+    //     vUv.x + sin(vUv.y * 10.0 * PI) * 0.1,
+    //     vUv.y + sin(vUv.x * 10.0 * PI) * 0.1
+    // );
+    // float strength = 1.0 - step(0.01, abs(distance(vUv2, vec2(0.5)) - 0.25));
+    // --------------------------------------------------------
+    // float strength = atan(vUv.x, vUv.y);
+    // --------------------------------------------------------
+    // float strength = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // --------------------------------------------------------
+    // float strength = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // strength /= PI * 2.0;
+    // strength += 0.5;
+    // --------------------------------------------------------
+    // float strength = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // strength = mod(strength / PI * 10.0, 1.0);
+    // --------------------------------------------------------
+    // float strength = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // strength = sin(strength * 10.0);
+    // --------------------------------------------------------
+    // float strength = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // float sinusoid = mod(strength / PI * 10.0, 1.0);
+    // float radius = 0.3 + sinusoid * 0.2;
+    // strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+    // --------------------------------------------------------
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // float sinusoid = sin(angle * 20.0);
+    // float radius = 0.3 + sinusoid * 0.03;
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+    // --------------------------------------------------------
+    // float strength = cnoise(vUv * 10.0);
+    // --------------------------------------------------------
+    // float strength = step(0.0, cnoise(vUv * 10.0));
+    // --------------------------------------------------------
+    // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+    // --------------------------------------------------------
+    // float strength = sin(abs(cnoise(vUv * 10.0)) * 20.0);
+    // --------------------------------------------------------
+    // float strength = sin(abs(cnoise(vUv * 10.0)) * 20.0);
+    // --------------------------------------------------------
+    // float strength = step(0.7, sin(abs(cnoise(vUv * 10.0)) * 20.0));
+    // --------------------------------------------------------
+    // float strength = step(0.7, sin(abs(cnoise(vUv * 10.0)) * 20.0));
+    strength = clamp(strength, 0.0, 1.0);
+    vec3 bgColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 mixColor = mix(bgColor, uvColor, strength);
+    gl_FragColor = vec4(mixColor, 1.0);
 }
