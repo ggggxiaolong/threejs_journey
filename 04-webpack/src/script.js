eb33f22d78b22e130408f1f656ffd2e4e637a5fb
MrTan <freemrtan@gmail.com>
2022-12-16 16:08:25 +0800

    31 Animated galaxy

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -3,21 +3,27 @@ import {
   AdditiveBlending,
   BufferAttribute,
   BufferGeometry,
+  CameraHelper,
   Clock,
   Color,
+  CubeTextureLoader,
+  DirectionalLight,
   Mesh,
+  MeshStandardMaterial,
   PerspectiveCamera,
   PlaneGeometry,
   Points,
   PointsMaterial,
   Scene,
   ShaderMaterial,
+  sRGBEncoding,
+  TextureLoader,
   Vector2,
   WebGLRenderer,
 } from "three";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import vertexShader from "./shaders/galaxy/vertex.glsl";
-import fragmentShader from "./shaders/galaxy/fragment.glsl";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { GUI } from "dat.gui";
 
 const size = {
@@ -30,76 +36,123 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new Scene();
 const gui = new GUI();
 const params = {};
+const cubeLoader = new CubeTextureLoader();
+const gltfLoader = new GLTFLoader();
+const textureLoader = new TextureLoader();
+// const glfLoader = new Gl
+const envTexture = cubeLoader
+  .setPath("/textures/environmentMaps/0/")
+  .load(["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]);
 
-params.size = 0.01;
-params.count = 100000;
+envTexture.encoding = THREE.sRGBEncoding;
+scene.background = envTexture;
+scene.environment = envTexture;
 
-const geometry = new BufferGeometry();
-const material = new ShaderMaterial({
-  // size: params.size,
-  depthWrite: false,
-  vertexColors: true,
-  blending: AdditiveBlending,
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
-  uniforms:{
-    uSize: {value: 8.0 * size.pixelRatio},
-    uTime: {value: 0.0},
-  }
+const mapTexture = textureLoader.load("/models/LeePerrySmith/color.jpg");
+mapTexture.encoding = THREE.sRGBEncoding;
+const normalTexture = textureLoader.load("/models/LeePerrySmith/normal.jpg");
+const material = new MeshStandardMaterial({
+  map: mapTexture,
+  normalMap: normalTexture,
 });
 
-params.branch = 3;
-params.radius = 3;
-params.power = 3;
-params.randomness = 0.2;
+const depthMaterial = new THREE.MeshDepthMaterial({
+  depthPacking: THREE.RGBADepthPacking,
+});
 
-const positions = new Float32Array(params.count * 3);
-const randomPosition = new Float32Array(params.count * 3);
-const colors = new Float32Array(params.count * 3);
-const scales = new Float32Array(params.count);
-const innerColor = new Color("#ff6030")
-const outsideColor = new Color("#1b3984")
-for (let i = 0; i < params.count; i++) {
-  const i3 = i * 3;
-  const angle = (((i % params.branch) * Math.PI) / 3) * 2;
-  const radius = Math.random() * params.radius;
+const customUniforms = {
+  uTime: { value: 0 },
+};
+material.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+      uniform float uTime;
+      mat2 get2dRotateMatrix(float _angle)
+      {
+          return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+      }
+    `
+  );
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+        #include <begin_vertex>
+        float angle = sin(position.y + uTime) * 0.4;
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+        transformed.xz = rotateMatrix * transformed.xz;
+    `,
+  );
+};
 
-  const randomX = Math.pow(Math.random(), params.power) * (Math.random() > 0.5 ? 1 : -1) * params.randomness * radius;
-  const randomY = Math.pow(Math.random(), params.power) * (Math.random() > 0.5 ? 1 : -1) * params.randomness * radius;
-  const randomZ = Math.pow(Math.random(), params.power) * (Math.random() > 0.5 ? 1 : -1) * params.randomness * radius;
-
-
-  positions[i3] = Math.cos(angle) * radius;
-  positions[i3 + 1] = 0.0;
-  positions[i3 + 2] = Math.sin(angle) * radius;
-
-  randomPosition[i3] = randomX;
-  randomPosition[i3 + 1] =  randomY;
-  randomPosition[i3 + 2] = randomZ;
-
-  const mixColor = innerColor.clone()
-  mixColor.lerp(outsideColor, radius / params.radius);
-  colors[i3] = mixColor.r;
-  colors[i3 + 1] = mixColor.g;
-  colors[i3 + 2] = mixColor.b;
-
-  scales[i] = Math.random();
+depthMaterial.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+      uniform float uTime;
+      mat2 get2dRotateMatrix(float _angle)
+      {
+          return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+      }
+    `
+  );
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+        #include <begin_vertex>
+        float angle = sin(position.y + uTime) * 0.4;
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+        transformed.xz = rotateMatrix * transformed.xz;
+    `,
+  );
+};
+function updateAllMaterial() {
+  scene.traverse((obj) => {
+    if (obj instanceof Mesh && obj.material instanceof MeshStandardMaterial) {
+      obj.material.envMapIntensity = 5;
+      obj.material.needsUpdate = true;
+      obj.castShadow = true;
+      obj.receiveShadow = true;
+    }
+  });
 }
-geometry.setAttribute("position", new BufferAttribute(positions, 3));
-geometry.setAttribute("color", new BufferAttribute(colors, 3));
-geometry.setAttribute("aRandom", new BufferAttribute(randomPosition, 3));
-geometry.setAttribute("aScale", new BufferAttribute(scales, 1));
 
-const points = new Points(geometry, material);
-scene.add(points);
+gltfLoader.load("/models/LeePerrySmith/LeePerrySmith.glb", (model) => {
+  const mesh = model.scene.children[0];
+  mesh.rotation.y = 0.5 * Math.PI;
+  mesh.material = material;
+  mesh.customDepthMaterial = depthMaterial;
+  scene.add(mesh);
+  updateAllMaterial();
+});
 
 const camera = new PerspectiveCamera(75, size.width / size.height, 0.01, 100);
-camera.position.set(-1, 2, 4);
+camera.position.set(4, 1, -5);
 scene.add(camera);
+
+const light = new DirectionalLight(0xffffff, 3);
+light.castShadow = true;
+light.shadow.camera.far = 15;
+light.shadow.mapSize.set(1024, 1024);
+light.position.set(2, 2, -2.25);
+scene.add(light);
+
+// const cameraHelper = new CameraHelper(light.shadow.camera);
+// scene.add(cameraHelper)
+
 const render = new WebGLRenderer({ canvas: canvas, antialias: true });
 render.setSize(size.width, size.height);
 render.setPixelRatio(size.devicePixelRatio);
-render.setClearColor(0x333333);
+render.physicallyCorrectLights = true;
+render.outputEncoding = THREE.sRGBEncoding;
+render.shadowMap.enabled = true;
+render.shadowMap.type = THREE.PCFShadowMap;
+render.toneMapping = THREE.ACESFilmicToneMapping;
+render.toneMappingExposure = 1;
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
@@ -113,14 +166,12 @@ window.addEventListener("resize", () => {
   render.setPixelRatio(size.devicePixelRatio);
   camera.aspect = size.width / size.height;
   camera.updateProjectionMatrix();
-
-  material.uniforms.uSize.value = 8.0 * size.pixelRatio;
 });
 
 const clock = new Clock();
 
 function update() {
-  material.uniforms.uTime.value = clock.getElapsedTime();
+  customUniforms.uTime.value = clock.getElapsedTime();
   render.render(scene, camera);
   controls.update();
   window.requestAnimationFrame(update);
